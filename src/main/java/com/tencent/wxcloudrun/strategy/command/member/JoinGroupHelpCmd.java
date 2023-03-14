4ab6a1e15e8aff604eb2cb23b754157e77205760
zztjay <zztjay@163.com>
2023-03-14 17:34:53 +0800

    加群优化

--- src/main/java/com/tencent/wxcloudrun/strategy/command/member/JoinGroupHelpCmd.java
+++ src/main/java/com/tencent/wxcloudrun/strategy/command/member/JoinGroupHelpCmd.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSONObject;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.LoginInfo;
 import com.tencent.wxcloudrun.model.Member;
+import com.tencent.wxcloudrun.service.CampService;
 import com.tencent.wxcloudrun.strategy.command.Command;
 import com.tencent.wxcloudrun.util.RegexUtils;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +21,8 @@ import java.util.List;
  */
 @Component
 public class JoinGroupHelpCmd implements Command<String> {
+    @Resource
+    CampService campService;
 
     @Override
     public boolean isMatch(String inputCmd) {
@@ -37,15 +41,19 @@ public class JoinGroupHelpCmd implements Command<String> {
 
     @Override
     public String resultFormat(JSONObject data, LoginInfo loginInfo) {
-        return new StringBuilder("欢迎加入，以下是打卡帮助：\n\n" +
-                "Step1： 输入“原体重xx斤”和“目标xx斤”，进行初始体重信息设置\n\n" +
-                "Step2：请按打卡模版进行打卡，勿改变每一行的打卡标题（比如：“运动：”），打卡内容请用“，”分隔，没有请输入“无”，或删除行\n\n" +
-                "\uD83D\uDC49今日体重：138斤\n" +
-                "⛹️\u200D♀️运动：走路5000步，力量训练60分钟\n" +
-                "\uD83C\uDF5E早餐：豆浆，包子，全麦面包\n" +
-                "\uD83C\uDF5A午餐：西红柿炒鸡蛋，炒牛肉，米饭\n" +
-                "\uD83C\uDF75晚餐：咖喱鸡肉饭\n" +
-                "\uD83C\uDF49加餐：无").toString();
+        // 存在减脂营的情况下才提示
+        if(loginInfo.getCampId() != null){
+            return new StringBuilder("欢迎加入，以下是打卡帮助：\n\n" +
+                    "Step1： 输入“原体重xx斤”和“目标xx斤”，进行初始体重信息设置\n\n" +
+                    "Step2：请按打卡模版进行打卡，勿改变每一行的打卡标题（比如：“运动：”），打卡内容请用“，”分隔，没有请输入“无”，或删除行\n\n" +
+                    "\uD83D\uDC49今日体重：138斤\n" +
+                    "⛹️\u200D♀️运动：走路5000步，力量训练60分钟\n" +
+                    "\uD83C\uDF5E早餐：豆浆，包子，全麦面包\n" +
+                    "\uD83C\uDF5A午餐：西红柿炒鸡蛋，炒牛肉，米饭\n" +
+                    "\uD83C\uDF75晚餐：咖喱鸡肉饭\n" +
+                    "\uD83C\uDF49加餐：无").toString();
+        }
+        return null;
     }
 
 
